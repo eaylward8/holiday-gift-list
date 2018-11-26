cc787766c73b2741f43c2a3f60da0e84c8f954e2
Erik Aylward <eaylward@mail.med.upenn.edu>
2018-11-26 10:43:58 -0500

    Sorting works except on initial page load

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -3,6 +3,13 @@
 class RecipientsController < ApplicationController
   def index
     @recipients = Recipient.includes(:gift).where(nice: params[:nice])
+    @recipients = @recipients.order("#{params[:sort]} #{params[:order]}") if order_results?
     render :index, layout: false
+  end
+
+  private
+
+  def order_results?
+    params[:sort].present? && params[:order].present?
   end
 end
--- app/javascript/controllers/gift_list_controller.js
+++ app/javascript/controllers/gift_list_controller.js
@@ -1,12 +1,54 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "container" ]
+  static targets = [ "container", "header" ]
 
-  async displayGifts(event) {
+  get niceParam() {
+    return this.data.get('niceParam')
+  }
+
+  set niceParam(param) {
+    this.data.set('niceParam', param)
+  }
+
+  handleNavSwitch(event) {
     const url = event.detail
-    const response = await fetch(url)
-    const html = await response.text()
+    this.niceParam = url.match(/nice=([0,1])/)[1]
+    this.render(url)
+  }
+
+  async sort(event) {
+    let url = event.currentTarget.dataset.url
+    const order = this.nextOrder(event.currentTarget)
+    // debugger
+    if (order === 'none') {
+      url = `${url.split('sort')[0]}nice=${this.niceParam}` // remove sort param
+    } else {
+      url = `${url}&order=${order}&nice=${this.niceParam}`
+    }
+    this.render(url)
+  }
+
+  nextOrder(target) {
+    const currentOrder = target.getAttribute('aria-sort')
+    if (currentOrder === 'none') { this.updateSorting(target, 'ascending'); return 'asc' }
+    if (currentOrder === 'ascending') { this.updateSorting(target, 'descending'); return 'desc' }
+    if (currentOrder === 'descending') { this.updateSorting(target, 'none'); return 'none' }
+  }
+
+  updateSorting(target, order) {
+    target.setAttribute('aria-sort', order)
+    let otherHeaders = this.headerTargets.filter(header => header !== target)
+    otherHeaders.forEach(header => header.setAttribute('aria-sort', 'none'))
+  }
+
+  async render(url) {
+    const html = await this.fetchData(url)
     this.containerTarget.innerHTML = html
+  }
+
+  async fetchData(url) {
+    const response = await fetch(url)
+    return await response.text()
   }
 }
